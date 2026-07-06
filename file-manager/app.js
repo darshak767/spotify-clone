e1467a8a14758e940702f6bcfbe7656991c82dc4
darsh <darsh@example.com>
2026-07-06 22:41:17 +0530

    Update file manager app

--- file-manager/app.js
+++ file-manager/app.js
@@ -1,17 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const URL = require("url");
 
 const server = http.createServer((req, res) => {
-  fs.readdir("files", (err, files) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end("Internal Server Error");
-      return;
-    } else if (req.url === "./files") {
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ files }));
-    }
-  });
+  // fs.readdir("files", (err, files) => {
+  //   if (err) {
+  //     res.statusCode = 500;
+  //     res.end("Internal Server Error");
+  //     return;
+  //   } else if (req.url === "./files") {
+  //     res.setHeader("Content-Type", "application/json");
+  //     res.end(JSON.stringify({ files }));
+  //   }
+  // });
+
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
 });
 
 server.listen(3000, () => {
